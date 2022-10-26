b783f48185df9a35ab636a4e210c355bb247ee3a
danifaso02 <danifaso02@gmail.com>
2022-10-26 10:58:52 +0200

    fine

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -3,3 +3,12 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
+const word1 = prompt("Inserire parola");
+const word2 = prompt("Inserire parola");
+
+if(word1.length > word2.length){
+    console.log(word1, word2);
+}
+else{
+    console.log(word1, word2);
+}
