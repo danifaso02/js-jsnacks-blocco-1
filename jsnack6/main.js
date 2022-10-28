1b1e331b079646249d24a4575ad3a08a605949f2
danifaso02 <danifaso02@gmail.com>
2022-10-28 10:48:14 +0200

    modifica if messo 1

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -3,12 +3,12 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
 
 // dichiarazione array e variabile somma
-const listNum = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"];
+const listNum = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 let sum = 0;
 
 // ciclo
 for (let i=0; i < listNum.length; i++){
-    if(i % 2 != 0){
+    if(i % 2 != 1){
         sum = sum + listNum[i];
     }
 }
